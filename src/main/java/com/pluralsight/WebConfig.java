132592e692caa7028513abc43e233489cbc92abb
bhchandra <bhanumchandra@gmail.com>
2016-11-05 17:32:17 -0500

    1. Intial work from SpringStarterProject
    Application to track events

---
+++ src/main/java/com/pluralsight/WebConfig.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc		//Basically the same as servlet-config.xml
+public class WebConfig {
+
+	
+}
